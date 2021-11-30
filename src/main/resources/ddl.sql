01da2382e7fc6f3313030b54c8fdffb089e47667
Leandro Aparecido <leandro@backbase.com>
2021-11-30 12:58:58 +0100

    DDL

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,12 +1,12 @@
 CREATE TABLE account_type (
   id INT,
+  account_id INT NOT NULL,
   name VARCHAR(255) NOT NULL,
   CONSTRAINT pk_account_type PRIMARY KEY (id)
 );
 
 CREATE TABLE account (
   id INT AUTO_INCREMENT,
-  type_id INT NOT NULL,
   name VARCHAR(255) NOT NULL,
   closed BIT NOT NULL,
   CONSTRAINT pk_account PRIMARY KEY (id)
